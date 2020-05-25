4395a6238fa4152fe18ec00fc66fc1254869b1c8
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-25 19:00:47 -0400

    Initial commit

---
+++ climbStairs.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+
+
+class Solution(object):
+    d
+
+    def climbStairs(self, n):
+        opt = {}
+        opt[1], opt[2] = 1, 2
+        k = 1
+        while k != n:
+            if k += 1 != n:
+                k += 1
+            if k += 2 != n:
+                k += 1
+        return k
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        4
+        1 + 1 + 1 + 1
+        2 + 1 + 1
+        2 + 2
+        1 + 1 + 2
+        1 + 2 + 1
+        permutate and add if its
+        not the
+        same as length
+
+        5
+        1 + 1 + 1 + 1 + 1
+        1 + 1 + 1 + 2
+        2 + 2 + 1
+        2 +
